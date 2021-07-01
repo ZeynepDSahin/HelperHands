1915897b6a23489b7d942d6bef5bf60b6c770445
Zeynep <zeynepdamlasah@gmail.com>
2021-07-01 12:18:20 -0500

    Latest Helper Hands App Version July 1st 12:18 PM

--- SecondViewController.swift
+++ SecondViewController.swift
@@ -8,7 +8,27 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    
+    @IBAction func chicagoFoodBanks(_ sender: UIButton) {
+        let chicagofoodbanks = URL (string : "https://www.chicagosfoodbank.org/find-food/")
+        UIApplication.shared.open (chicagofoodbanks!)
+    }
+    
+    @IBAction func irvingFoodBank(_ sender: UIButton) {
+        let irvingfoodbank = URL (string : "https://irvingparkfoodpantry.com/")
+        UIApplication.shared.open(irvingfoodbank!)
+    }
+    
+    @IBAction func lakeViewPantryButton(_ sender: UIButton) {
+        let lakeviewpantrybutton = URL (string : "https://www.lakeviewpantry.org/?gclid=CjwKCAjwrPCGBhALEiwAUl9X0w-7a5adROx8_xr0FaAZ5JQgg10b-miV33fYNM4eaDim1ybhxsFv7BoCjaEQAvD_BwE")
+        UIApplication.shared.open(lakeviewpantrybutton!)
+    }
+    
+    @IBAction func loveFridgeButton(_ sender: UIButton) {
+        let lovefridgebutton = URL (string : "https://www.thelovefridge.com/locations")
+        UIApplication.shared.open(lovefridgebutton!)
+    }
     @IBAction func chicagoFoodBankButton(_ sender: UIButton) {
         let foodbanksnear = URL (string : "https://www.google.com/maps/search/food+banks+near+me/@41.9270621,-87.7533758,13z/data=!3m1!4b1")
         UIApplication.shared.open(foodbanksnear!)
--- ThirdViewController.swift
+++ ThirdViewController.swift
@@ -9,10 +9,30 @@ import UIKit
 
 class ThirdViewController: UIViewController {
 
-    @IBAction func dallasFoodBankButton(_ sender: UIButton) {
-        let foodbanksnear = URL (string : "https://www.google.com/maps/search/food+banks+near+me/@41.9270621,-87.7533758,13z/data=!3m1!4b1")
-        UIApplication.shared.open (foodbanksnear!)
+    @IBAction func dallasfoodBanksNearButton(_ sender: UIButton) {
+        let dallasfoodbanksnearbutton = URL (string : "https://www.google.com/maps/search/food+banks+near+me/@41.9270621,-87.7533758,13z/data=!3m1!4b1")
+        UIApplication.shared.open (dallasfoodbanksnearbutton!)
     }
+    @IBAction func foodPantries(_ sender: UIButton) {
+        let foodpantries = URL (string : "https://www.foodpantries.org/ci/tx-dallas")
+        UIApplication.shared.open (foodpantries!)
+    }
+    
+    @IBAction func dallasLife(_ sender: UIButton) {
+        let dallaslife = URL (string : "https://dallaslife.org/services/meals-shelter/")
+        UIApplication.shared.open (dallaslife!)
+    }
+    
+    @IBAction func fridgeButton(_ sender: UIButton) {
+        let fridgebutton = URL (string : "https://www.thepeoplesfridge.com/locations")
+        UIApplication.shared.open (fridgebutton!)
+    }
+    
+    @IBAction func feedThePeople(_ sender: UIButton) {
+        let feedthepeople = URL (string : "https://instagram.com/feedthepeopledallas?utm_medium=copy_link")
+        UIApplication.shared.open (feedthepeople!)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
